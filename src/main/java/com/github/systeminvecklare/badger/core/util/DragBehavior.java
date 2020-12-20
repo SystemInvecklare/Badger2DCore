8a823720e6af004f0c2967ceeff1775c6d48165b
Mattias Selin <mattias.selin@eyevinn.se>
2020-12-20 22:16:38 +0100

    Replaced DragBehavior with a more useful version

--- src/main/java/com/github/systeminvecklare/badger/core/util/DragBehavior.java
+++ src/main/java/com/github/systeminvecklare/badger/core/util/DragBehavior.java
@@ -4,7 +4,6 @@ import com.github.systeminvecklare.badger.core.graphics.components.FlashyEngine;
 import com.github.systeminvecklare.badger.core.graphics.components.core.IDragEventListener;
 import com.github.systeminvecklare.badger.core.graphics.components.core.IReleaseEventListener;
 import com.github.systeminvecklare.badger.core.graphics.components.core.ITic;
-import com.github.systeminvecklare.badger.core.graphics.components.movieclip.IMovieClipContainer;
 import com.github.systeminvecklare.badger.core.graphics.components.movieclip.behavior.Behavior;
 import com.github.systeminvecklare.badger.core.graphics.components.transform.IReadableTransform;
 import com.github.systeminvecklare.badger.core.graphics.components.transform.ITransform;
@@ -14,114 +13,213 @@ import com.github.systeminvecklare.badger.core.math.IReadablePosition;
 import com.github.systeminvecklare.badger.core.math.Position;
 import com.github.systeminvecklare.badger.core.math.Vector;
 import com.github.systeminvecklare.badger.core.pooling.EasyPooler;
-import com.github.systeminvecklare.badger.core.pooling.IPoolManager;
+import com.github.systeminvecklare.badger.core.pooling.IPool;
 
 public class DragBehavior extends Behavior {
-	private boolean beingDragged = false;
-	private Position lastPosition;
-	private Vector currentOffset;
+	private Vector positionOffset;
+	private Vector tentativeOffset;
+	private Position dragStart;
+	private Position lastMeaningfulMousePos;
+	private boolean dragging = false;
+	private CancellableReleaseEventListener currentReleaseEventListener = null;
+	private CancellableDragEventListener currentDragEventListener = null;
+	
 	private boolean disposed = false;
-	private IMovieClipContainer parentOfBound;
 	
 	@Override
+	public void onClick(IClickEvent clickEvent) {
+		if(!dragging && !clickEvent.isConsumed() && isValidClick(clickEvent)) {
+			clickEvent.consume();
+			dragging = true;
+			
+			onStartDrag(clickEvent.getPosition());
+			
+			convertPosition(clickEvent.getPosition(), dragStart); // Global pos right?
+			lastMeaningfulMousePos.setTo(clickEvent.getPosition());
+			
+			
+			
+			cancelDragAndReleaseListeners();
+			currentDragEventListener = new CancellableDragEventListener() {
+				@Override
+				protected void onDragNotCancelled(IPointerStateEvent e) {
+					if(disposed) {
+						return;
+					}
+					
+					onDragged(e.getPosition(), true);
+					
+					lastMeaningfulMousePos.setTo(e.getPosition());
+				}
+			};
+			currentReleaseEventListener = new CancellableReleaseEventListener() {
+				@Override
+				protected void onReleaseNotCancelled(IPointerStateEvent e) {
+					if(disposed) {
+						return;
+					}
+					lastMeaningfulMousePos.setTo(e.getPosition());
+					
+					forceRelease();
+				}
+			};
+			clickEvent.addDragListener(currentDragEventListener);
+			clickEvent.addReleaseListener(currentReleaseEventListener);
+		}
+	}
+	
+	private void cancelDragAndReleaseListeners() {
+		if(currentDragEventListener != null) {
+			currentDragEventListener.cancel();
+			currentDragEventListener = null;
+		}
+		if(currentReleaseEventListener != null) {
+			currentReleaseEventListener.cancel();
+			currentReleaseEventListener = null;
+		}
+	}
+
+	public final void forceRelease() {
+		if(disposed) {
+			return;
+		}
+		cancelDragAndReleaseListeners();
+		
+		updateTentativeOffset();
+		dragging = false;
+		positionOffset.add(tentativeOffset);
+		tentativeOffset.setToOrigin();
+		
+		onStopDrag(lastMeaningfulMousePos);
+	}
+	
+	protected void onStartDrag(IReadablePosition globalPosition) {
+	}
+	
+	protected void onStopDrag(IReadablePosition globalPosition) {
+	}
+	
+	protected void onDragged(IReadablePosition globalPosition, boolean mouseMoved) {
+	}
+	
+	public Vector harvestPositionOffset(Vector result) {
+		if(disposed) {
+			return result;
+		}
+		if(result != null) {
+			result.add(positionOffset);
+		}
+		positionOffset.setToOrigin();
+		if(dragging) {
+			if(result != null) {
+				updateTentativeOffset();
+				result.add(tentativeOffset);
+			}
+			tentativeOffset.setToOrigin();
+			dragStart.setTo(lastMeaningfulMousePos);
+		}
+		return result;
+	}
+
+	protected final void updateTentativeOffset() {
+		EasyPooler ep = EasyPooler.obtainFresh();
+		try {
+			Position currentPos = ep.obtain(Position.class);
+			convertPosition(lastMeaningfulMousePos, currentPos);
+			dragStart.vectorTo(currentPos, tentativeOffset);
+		} finally {
+			ep.freeAllAndSelf();
+		}
+	}
+
+	@Override
+	public void think(ITic tic) {
+		if(dragging) {
+			updateTentativeOffset();
+			onDragged(lastMeaningfulMousePos, false);
+		}
+	}
+
+	protected boolean isValidClick(IClickEvent clickEvent) {
+		return true;
+	}
+
+	@Override
 	public void init() {
-		super.init();
-		IPoolManager pm = FlashyEngine.get().getPoolManager();
-		this.lastPosition = pm.getPool(Position.class).obtain().setToOrigin();
-		this.currentOffset = pm.getPool(Vector.class).obtain().setToOrigin();
-		applyConstraints(currentOffset);
-		this.parentOfBound = getBound().getParent();
+		IPool<Vector> vectorPool = FlashyEngine.get().getPoolManager().getPool(Vector.class);
+		IPool<Position> positionPool = FlashyEngine.get().getPoolManager().getPool(Position.class);
+		positionOffset = vectorPool.obtain().setToOrigin();
+		tentativeOffset = vectorPool.obtain().setToOrigin();
+		dragStart = positionPool.obtain().setToOrigin();
+		lastMeaningfulMousePos = positionPool.obtain().setToOrigin();
 	}
 	
 	@Override
 	public void dispose() {
-		this.lastPosition.free();
-		this.lastPosition = null;
-		this.currentOffset.free();
-		this.currentOffset = null;
-		super.dispose();
+		cancelDragAndReleaseListeners();
 		disposed = true;
+		positionOffset.free();
+		positionOffset = null;
+		tentativeOffset.free();
+		tentativeOffset = null;
+		dragStart.free();
+		dragStart = null;
+		lastMeaningfulMousePos.free();
+		lastMeaningfulMousePos = null;
 	}
 	
-	@Override
-	public void onClick(IClickEvent clickEvent) {
-		if(disposed) {
-			return;
-		}
-		if(!beingDragged) {
-			if(isCorrectButton(clickEvent) && !clickEvent.isConsumed()) {
-				clickEvent.consume();
-				beingDragged = true;
-				
-				onPosition(clickEvent.getPosition());
-				
-				clickEvent.addDragListener(new IDragEventListener() {
-					@Override
-					public void onDrag(IPointerStateEvent e) {
-						onPosition(e.getPosition());
-					}
-				});
-				clickEvent.addReleaseListener(new IReleaseEventListener() {
-					@Override
-					public void onRelease(IPointerStateEvent e) {
-						onPosition(e.getPosition());
-						beingDragged = false;
-					}
-				});
-			}
-		}
-	}
-	
-	private void onPosition(IReadablePosition readablePosition) {
-		if(!disposed) {
-			this.lastPosition.setTo(readablePosition);
-			EasyPooler ep = EasyPooler.obtainFresh();
-			try {
-				updateCurrentPosition(getPositionInParent(readablePosition, ep));
-			} finally {
-				ep.freeAllAndSelf();
-			}
-		}
-	}
-	
-	private Position getPositionInParent(IReadablePosition position, EasyPooler ep) {
-		return getPositionInParent(position, ep.obtain(Position.class));
-	}
-	
-	private Position getPositionInParent(IReadablePosition position, Position result) {
+	protected void convertPosition(IReadablePosition globalPosition, Position result) {
 		EasyPooler ep = EasyPooler.obtainFresh();
 		try {
-			return result.setTo(parentOfBound.toLocalTransform(ep.obtain(ITransform.class).setToIdentity().setPosition(position)).getPosition());
+			IReadablePosition localPos = getBound().getParent().toLocalTransform(ep.obtain(ITransform.class).setToIdentity().setPosition(globalPosition)).getPosition();
+			result.setTo(localPos);
 		} finally {
 			ep.freeAllAndSelf();
 		}
 	}
 	
-	private void updateCurrentPosition(IReadablePosition position) {
-		currentOffset.setTo(position);
-		applyConstraints(currentOffset);
+	@Override
+	public ITransform getTransform(IReadableTransform transform, ITransform result) {
+		result = result.setTo(transform).addToPosition(positionOffset);
+		if(dragging) {
+			return result.addToPosition(tentativeOffset);
+		}
+		return result;
 	}
 	
-	@Override
-	public void think(ITic tic) {
-		if(beingDragged && !disposed) {
-			onPosition(lastPosition);
+	private abstract static class CancellableReleaseEventListener implements IReleaseEventListener {
+		private boolean cancelled = false;
+
+		@Override
+		public final void onRelease(IPointerStateEvent e) {
+			if(cancelled) {
+				return;
+			}
+			onReleaseNotCancelled(e);
+		}
+
+		protected abstract void onReleaseNotCancelled(IPointerStateEvent e);
+		
+		public void cancel() {
+			cancelled = true;
 		}
 	}
 	
-	protected void applyConstraints(Vector currentOffset) {
-	}
+	private abstract static class CancellableDragEventListener implements IDragEventListener {
+		private boolean cancelled = false;
+		
+		@Override
+		public final void onDrag(IPointerStateEvent e) {
+			if(cancelled) {
+				return;
+			}
+			onDragNotCancelled(e);
+		}
 
-	protected boolean isCorrectButton(IClickEvent clickEvent) {
-		return true;
-	}
-	
-	@Override
-	public ITransform getTransform(IReadableTransform transform, ITransform result) {
-		if(disposed) {
-			return null;
-		} else {
-			return result.setTo(transform).addToPosition(currentOffset);
+		protected abstract void onDragNotCancelled(IPointerStateEvent e);
+		
+		public void cancel() {
+			cancelled = true;
 		}
 	}
 }

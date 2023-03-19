f6cfbdebfd2116bbed3ecea35a94eb614da5ccee
Mattias Selin <mattias.selin@gmail.com>
2023-03-19 17:30:11 +0100

    Additions to GeometryUtil

--- src/main/java/com/github/systeminvecklare/badger/core/util/GeometryUtil.java
+++ src/main/java/com/github/systeminvecklare/badger/core/util/GeometryUtil.java
@@ -1,5 +1,7 @@
 package com.github.systeminvecklare.badger.core.util;
 
+import com.github.systeminvecklare.badger.core.widget.IRectangle;
+
 public class GeometryUtil {
 	public static boolean isInRectangle(float px, float py, float x, float y, float width,float height)
 	{
@@ -9,6 +11,10 @@ public class GeometryUtil {
 	public static boolean isInRectangle(double px, double py, double x, double y, double width,double height)
 	{
 		return px >= x && py >= y && px < x+width && py < y+height;
+	}
+	
+	public static boolean isInRectangle(float px, float py, IRectangle rectangle) {
+		return isInRectangle(px, py, rectangle.getX(), rectangle.getY(), rectangle.getWidth(), rectangle.getHeight());
 	}
 	
 	public static boolean isInCircle(float px, float py, float x, float y, float radius)
@@ -23,5 +29,11 @@ public class GeometryUtil {
 		double dx = px-x;
 		double dy = py-y;
 		return dx*dx + dy*dy < radius*radius;
+	}
+	
+	public static boolean isInOval(float px, float py, float x, float y, float radiusX, float radiusY) {
+		float dx = (px-x)/radiusX;
+		float dy = (py-y)/radiusY;
+		return dx*dx + dy*dy < 1f;
 	}
 }
